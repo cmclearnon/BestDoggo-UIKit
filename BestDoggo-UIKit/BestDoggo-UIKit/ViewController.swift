4985e66af814a9d458c12be1f0ba7ea3edbf661d
Chris McLearnon <chrismclearnon@icloud.com>
2020-07-16 13:53:40 +0100

    Initial commit of new Xcode project

---
+++ BestDoggo-UIKit/BestDoggo-UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestDoggo-UIKit
+//
+//  Created by Chris McLearnon on 16/07/2020.
+//  Copyright © 2020 chrismclearnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BestDoggo-UIKit/BestDoggo-UIKitTests/BestDoggo_UIKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  BestDoggo_UIKitTests.swift
+//  BestDoggo-UIKitTests
+//
+//  Created by Chris McLearnon on 16/07/2020.
+//  Copyright © 2020 chrismclearnon. All rights reserved.
+//
+
+import XCTest
+@testable import BestDoggo_UIKit
+
+class BestDoggo_UIKitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
